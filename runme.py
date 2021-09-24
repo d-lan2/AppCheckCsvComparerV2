9e0663d5ecb0296ccc39a9b0f723be612fe6abc0
dylan <dylan.riley@hotmail.com>
2021-09-24 12:38:09 +0100

    Add example of running the script

--- runme.py
+++ runme.py
@@ -1,4 +1,8 @@
 from src.main import runMain
 
+runMain("C:/Users/dylan/Downloads/Aug_Scan.csv",
+"C:/Users/dylan/Downloads/Sept_Scan.csv",
+"./output/OutputNew.csv", ",")
+
 #The below are just placeholders
-runMain("oldScanLocationHere", "newScanLocationHere", "./output/Output.csv", ",")
+#runMain("oldScanLocationHere", "newScanLocationHere", "./output/Output.csv", ",")
