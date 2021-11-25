4ab0c5ab8e904a598c931007e7a165df9349010b
dylan riley <dylan.riley@secarma.com>
2021-11-25 14:14:16 +0000

    Add some self documenting code

--- runme.py
+++ runme.py
@@ -1,8 +1,8 @@
 from src.main import runMain
 
-runMain("C:/Users/dylan/Downloads/Aug_Scan.csv",
-"C:/Users/dylan/Downloads/Sept_Scan.csv",
-"./output/OutputNew.csv", ",")
+runMain("C:/Users/dylan/Documents/Secarma/Reports/LMS Monthly/november/comparer/oct.csv",
+"C:/Users/dylan/Documents/Secarma/Reports/LMS Monthly/november/comparer/nov.csv",
+"C:/Users/dylan/Documents/Secarma/Reports/LMS Monthly/november/comparer/difference.csv", ",")
 
 #The below are just placeholders
 #runMain("oldScanLocationHere", "newScanLocationHere", "./output/Output.csv", ",")
